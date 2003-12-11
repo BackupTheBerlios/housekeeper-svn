633afdbc8a6c7f74cc39f80e9c5a06119e86fe96
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2003-12-11 16:20:27 +0000

    Initial commit

---
+++ Housekeeper/trunk/housekeeper/src/java/net/sourceforge/housekeeper/Test.java
@@ -0,0 +1,19 @@
+package net.sourceforge.housekeeper;
+
+/**
+ * @author Adrian Gygax
+ */
+public class Test
+{
+
+	/**
+	 * 
+	 * 
+	 */
+	public Test()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
