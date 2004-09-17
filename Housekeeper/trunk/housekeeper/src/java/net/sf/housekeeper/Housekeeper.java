0710f70f0ccd5ba26cd49e8e7a41ae01222bd99d
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2004-09-17 15:43:41 +0000

    Update version to 0.1.2

--- Housekeeper/trunk/housekeeper/src/java/net/sf/housekeeper/Housekeeper.java
+++ Housekeeper/trunk/housekeeper/src/java/net/sf/housekeeper/Housekeeper.java
@@ -35,7 +35,7 @@ public final class Housekeeper
 {
 
     /** The version of the last release. */
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
 
     /**
      * Prevents instances of this class.
