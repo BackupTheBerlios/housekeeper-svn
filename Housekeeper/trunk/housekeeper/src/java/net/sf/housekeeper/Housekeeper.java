138eab4a9b2efabcd3f7511ae3c121dfcc08bff2
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2004-11-18 18:04:44 +0000

    Fixed application startup for Windows

--- Housekeeper/trunk/housekeeper/src/java/net/sf/housekeeper/Housekeeper.java
+++ Housekeeper/trunk/housekeeper/src/java/net/sf/housekeeper/Housekeeper.java
@@ -36,7 +36,7 @@ public final class Housekeeper
 {
 
     /** The version of the last release. */
-    public static final String VERSION = "0.1.2";
+    public static final String VERSION = "0.1.2a";
 
     /**
      * Prevents instances of this class.
