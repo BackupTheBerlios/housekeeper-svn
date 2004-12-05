5a0760ba695ae3c8508c697bea78e4ff0e9a3786
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2004-12-05 20:43:52 +0000

    Prepared release of v0.2.0

--- Housekeeper/trunk/housekeeper/dist/housekeeper.bat
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.bat
@@ -1,4 +1,4 @@
-java -cp housekeeper-0.2.0-SNAPSHOT.jar;lib\binding-1.0b5.jar;lib\glazedlists-0.9.3.jar;lib\renderpack-0.0.1.jar;lib\commons-lang-2.0.jar;lib\commons-logging-1.0.4.jar;lib\commons-configuration-1.0.jar;lib\commons-collections-3.1.jar;lib\forms-1.0.5.jar;lib\jdom-1.0.jar;lib\looks-1.2.2.jar net.sf.housekeeper.ApplicationController
+java -cp housekeeper-0.2.0.jar;lib\glazedlists-0.9.3.jar;lib\commons-lang-2.0.jar;lib\commons-logging-1.0.4.jar;lib\commons-configuration-1.0.jar;lib\commons-collections-3.1.jar;lib\forms-1.0.5.jar;lib\jdom-1.0.jar;lib\looks-1.2.2.jar net.sf.housekeeper.ApplicationController
 
 rem Add the following parameters for debugging:
 rem -ea -Djava.util.logging.config.file=etc/logging.properties
