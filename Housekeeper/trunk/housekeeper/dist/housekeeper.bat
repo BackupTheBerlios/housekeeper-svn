4725306fd85337b9ad2fc357148ab40e35d5976e
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2005-01-16 18:43:34 +0000

    Using a customized maven-jar-plugin which manages to specify the dependencies in the Jar-Manifest

--- Housekeeper/trunk/housekeeper/dist/housekeeper.bat
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.bat
@@ -1,4 +1,4 @@
-java -cp housekeeper-0.2.1.jar;lib\glazedlists-0.9.4.jar;lib\commons-lang-2.0.jar;lib\commons-logging-1.0.4.jar;lib\commons-configuration-1.0.jar;lib\commons-collections-forconfig-1.0.jar;lib\forms-1.0.5.jar;lib\jdom-1.0.jar;lib\looks-1.2.2.jar net.sf.housekeeper.ApplicationController
+java -jar housekeeper-0.2.1.jar
 
-rem Add the following parameters for debugging:
-rem -ea -Djava.util.logging.config.file=etc/logging.properties
+rem Use this line for debugging:
+rem java -ea -Djava.util.logging.config.file=etc/logging.properties -jar housekeeper-0.2.1.jar
--- Housekeeper/trunk/housekeeper/dist/housekeeper.sh
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-#Original version of this script from http://azureus.sf.net
 
-# get the app dir
-PROGRAM_DIR=`dirname "$0"`
-PROGRAM_DIR=`cd "$PROGRAM_DIR"; pwd`
-
-cd ${PROGRAM_DIR}
-
-# build the classpath
-for FILE in *.jar; do CLASSPATH="${CLASSPATH}:${FILE}"; done
-for FILE in lib/*.jar; do CLASSPATH="${CLASSPATH}:${FILE}"; done
-
-
-echo "java -cp ${CLASSPATH} net.sf.housekeeper.ApplicationController '$@'"
-java -cp ${CLASSPATH} net.sf.housekeeper.ApplicationController "$@"
-#java -ea -Djava.util.logging.config.file=etc/logging.properties -cp ${CLASSPATH} net.sf.housekeeper.ApplicationController "$@"
+java -jar housekeeper-0.2.1.jar
+#java -ea -Djava.util.logging.config.file=etc/logging.properties -jar housekeeper-0.2.1.jar
