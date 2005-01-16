f1f6ae9be820516af54cd16ab0c7b1797ef3aa83
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2005-01-16 20:55:42 +0000

    Use Ant to generate startup scripts from templates

--- Housekeeper/trunk/housekeeper/dist/housekeeper.sh
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -jar housekeeper-0.2.1.jar
-#java -ea -Djava.util.logging.config.file=etc/logging.properties -jar housekeeper-0.2.1.jar
+java -jar @@HOUSEKEEPER_JAR@@
+#java -ea -Djava.util.logging.config.file=etc/logging.properties -jar @@HOUSEKEEPER_JAR@@
