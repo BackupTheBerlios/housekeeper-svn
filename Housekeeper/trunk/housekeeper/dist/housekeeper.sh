f1bd61792c0a4b979c3e975bc5380af4e990c926
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2005-04-06 19:40:35 +0000

    Change to current working directory to fix execution form file managers. Fixes bug #3716

--- Housekeeper/trunk/housekeeper/dist/housekeeper.sh
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#Original version of this script from http://azureus.sf.net
+
+# get the app dir
+PROGRAM_DIR=`dirname "$0"`
+PROGRAM_DIR=`cd "$PROGRAM_DIR"; pwd`
+
+cd ${PROGRAM_DIR}
 
 java -jar @@HOUSEKEEPER_JAR@@
