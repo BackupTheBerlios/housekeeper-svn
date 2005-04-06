9851965034c2a8afabc8d957b322b222ba4bf52c
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2005-04-06 19:21:35 +0000

    Don't distribute logging.properties anymore

--- Housekeeper/trunk/housekeeper/dist/housekeeper.bat
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.bat
@@ -1,4 +1,1 @@
 java -jar @@HOUSEKEEPER_JAR@@
-
-rem Use this line for debugging:
-rem java -ea -Djava.util.logging.config.file=etc/logging.properties -jar @@HOUSEKEEPER_JAR@@
--- Housekeeper/trunk/housekeeper/dist/housekeeper.sh
+++ Housekeeper/trunk/housekeeper/dist/housekeeper.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 java -jar @@HOUSEKEEPER_JAR@@
-#java -ea -Djava.util.logging.config.file=etc/logging.properties -jar @@HOUSEKEEPER_JAR@@
