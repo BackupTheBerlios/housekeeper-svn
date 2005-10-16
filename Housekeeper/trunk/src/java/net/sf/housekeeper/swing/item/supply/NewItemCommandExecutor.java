cbea163990e7cb2c48a8d43f72f83d8dc4b61dee
notizklotz <notizklotz@00e2896f-6b00-0410-8efc-e62f505e3022>
2005-10-16 21:06:32 +0000

    Fixed command not enabled

--- Housekeeper/trunk/src/java/net/sf/housekeeper/swing/item/supply/NewItemCommandExecutor.java
+++ Housekeeper/trunk/src/java/net/sf/housekeeper/swing/item/supply/NewItemCommandExecutor.java
@@ -41,6 +41,11 @@ public final class NewItemCommandExecutor extends
 
     private ItemDAO<ExpirableItem> itemDAO;
 
+    public NewItemCommandExecutor()
+    {
+        setEnabled(true);
+    }
+    
     /*
      * (non-Javadoc)
      * 
